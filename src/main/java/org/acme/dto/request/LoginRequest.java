88e7b6def71f72f3951d78a428ee56301472bc0e
AllanGaBRs <allanmediogames@gmail.com>
2026-05-14 09:40:37 -0300

    feat(validation): add size constraints to login request

--- src/main/java/org/acme/dto/request/LoginRequest.java
+++ src/main/java/org/acme/dto/request/LoginRequest.java
@@ -2,14 +2,17 @@ package org.acme.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
 
         @Email
         @NotBlank
+        @Size(max = 100)
         String email,
 
         @NotBlank
+        @Size(max = 100)
         String password
 
 ) {
